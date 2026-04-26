3669ca5ad8ea4e6b88f7d804ff0741948d02afa3
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-04-26 13:46:00 +0600

    Get All Doctors done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoute } from "../module/user/user.route";
+import { DoctorRoute } from "../module/doctor/doctor.router";
 
 const router = Router();
 
 router.use('/auth',AuthRoutes)
 router.use('/specialties',SpecialtyRoutes)
-router.use('/doctors',UserRoute)
+router.use('/users',UserRoute)
+router.use('/doctors',DoctorRoute)
 
 export const IndexRoutes = router;
 
