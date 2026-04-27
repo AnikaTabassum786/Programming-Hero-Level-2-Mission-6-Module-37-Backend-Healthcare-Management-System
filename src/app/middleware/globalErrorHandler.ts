6b792747099cc5f1bdd5290d94f22f4c46b303c6
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-04-27 22:46:57 +0600

    Creating Zod Middleware And Creating Zod Error Handler For Global Error Handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -5,39 +5,42 @@ import { envVars } from "../../config/env";
 import { error } from "node:console";
 import status from "http-status";
 import z from "zod";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
+import { handleZodError } from "../errorHelpers/handleZodError";
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-    interface TErrorSources {
-        path: string;
-        message: string
-    }
 
     if (envVars.NODE_ENV === 'development') {
         console.log('Error from global error handler', err)
     }
 
 
-    const errorSource: TErrorSources[] = []
+    let errorSources: TErrorSources[] = []
     let statusCode: number = status.INTERNAL_SERVER_ERROR;
     let message: string = "Internal Server Error";
 
     if (err instanceof z.ZodError) {
-        statusCode = status.BAD_REQUEST;
-        message = "Zod Validation Error";
 
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number
+        message = simplifiedError.message
+        errorSources = [...simplifiedError.errorSources]
 
         err.issues.forEach(issue => {
-            errorSource.push({
+            errorSources.push({
                 path: issue.path.join(" => "),
                 message: issue.message
             })
         })
     }
 
-    res.status(statusCode).json({
+     const errorResponse: TErrorResponse = {
         success: false,
         message: message,
-        error: err.message
-    })
+        errorSources,
+        error: envVars.NODE_ENV === 'development' ? err : undefined
+    }
+
+    res.status(statusCode).json(errorResponse);
 }
