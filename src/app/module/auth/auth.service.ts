9b7732a75e9f6c0c46c13e1052cb0c129249dae7
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-04-28 21:13:45 +0600

    Creating Access Token And Refresh Token With JWT During Login User And Patient Registration

--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { UserStatus } from "../../../generated/prisma/enums";
 import AppError from "../../errorHelpers/AppError";
 import { auth } from "../../lib/auth";
 import { prisma } from "../../lib/prisma";
+import { tokenUtils } from "../../utils/token";
 
 
 interface IRegisterPatientPayload{
@@ -85,7 +86,31 @@ const loginUser= async(payload:ILoginUserPayload) =>{
         throw new AppError(status.NOT_FOUND,"User is deleted")
     }
 
-    return data;
+    const accessToken = tokenUtils.getAccessToken({
+        userId: data.user.id,
+        role:data.user.role,
+        name:data.user.name,
+        email:data.user.email,
+        status:data.user.status,
+        isDeleted:data.user.isDeleted,
+        emailVerified:data.user.emailVerified
+    })
+
+    const refreshToken = tokenUtils.getRefreshToken({
+         userId: data.user.id,
+        role:data.user.role,
+        name:data.user.name,
+        email:data.user.email,
+        status:data.user.status,
+        isDeleted:data.user.isDeleted,
+        emailVerified:data.user.emailVerified
+    })
+
+    return {
+        ...data,
+        accessToken,
+        refreshToken
+    }
 }
 
 export const AuthService={
