9ed394199a81f5bd31310dad34f38f00135bbd0b
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-04-29 11:26:28 +0600

    Setting The Access Token, Refresh Token And Better Auth Session Token In Response Cookies And Configuring Session Token Of Better Auth contiboue

---
+++ src/app/utils/cookie.ts
@@ -0,0 +1,23 @@
+
+import { Request, Response,CookieOptions } from "express";
+
+
+const setCookie =(res:Response,key:string,values:string,options:CookieOptions)=>{
+  res.cookie(key,values,options)
+}
+
+const getCookie=(req:Request,key:string)=>{
+    return req.cookies([key])
+}
+
+const clearCookie =(res:Response,key:string,options:CookieOptions)=>{
+    res.clearCookie(key,options)
+}
+
+
+export const CookieUtils = {
+    setCookie,
+    getCookie,
+    clearCookie,
+}
+
