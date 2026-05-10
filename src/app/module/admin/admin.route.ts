821b38ea3aa7baea4a713bc7f540cd9d8e3e0890
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-05-10 13:00:46 +0600

    Soft delete is not done

--- src/app/module/admin/admin.route.ts
+++ src/app/module/admin/admin.route.ts
@@ -3,9 +3,11 @@ import { AdminController } from "./admin.controller";
 import { checkAuth } from "../../middleware/checkAuth";
 import { Role } from "../../../generated/prisma/enums";
 
+
 const router = Router();
 router.get("/",checkAuth(Role.ADMIN,Role.SUPER_ADMIN), AdminController.getAllAdmin)
 router.get("/:id",checkAuth(Role.ADMIN,Role.SUPER_ADMIN),AdminController.getAdminById)
 router.patch("/:id",checkAuth(Role.SUPER_ADMIN),AdminController.updateAdmin)
+// router.delete("/:id",checkAuth(Role.SUPER_ADMIN),AdminController.deleteAdmin)
 
 export const AdminRoute = router;
