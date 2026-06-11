b64f55570d745c87e5d1f7f9c6b5bb7d92934cb3
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-06-11 13:03:28 +0600

    Creating Create Schedule API

--- src/app/middleware/validateRequest.ts
+++ src/app/middleware/validateRequest.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express"
 import z from "zod"
 
-export const validationRequest = (ZodSchema: z.ZodObject) => {
+export const validateRequest  = (ZodSchema: z.ZodObject) => {
     return (req: Request, res: Response, next: NextFunction) => {
 
         if (req.body.data) {
