6a0443386173766b0d98964dcf5ed674226565ab
AnikaTabassum217 <tabassum.anika579@gmail.com>
2026-06-12 13:36:05 +0600

    Completing Schedule APIs And Creating Doctor Schedule APIs

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,8 @@ import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoute } from "../module/user/user.route";
 import { DoctorRoute } from "../module/doctor/doctor.router";
 import { AdminRoute } from "../module/admin/admin.route";
+import { scheduleRoutes } from "../module/schedule/schedule.route";
+import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
 
 const router = Router();
 
@@ -12,6 +14,9 @@ router.use('/specialties',SpecialtyRoutes)
 router.use('/users',UserRoute)
 router.use('/doctors',DoctorRoute)
 router.use('/admins',AdminRoute)
+router.use("/schedules", scheduleRoutes)
+router.use("/doctor-schedules", DoctorScheduleRoutes)
+// router.use("/appointments", AppointmentRoutes)
 
 export const IndexRoutes = router;
 
